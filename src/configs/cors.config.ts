0874335478fda38f06a9f9724bb804c377903b54
Default <Default>
2026-01-26 10:30:13 +0530

    FB admin config update

--- src/configs/cors.config.ts
+++ src/configs/cors.config.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from "cors";
 import "dotenv/config";
 
 export const corsConfigs: CorsOptions = {
-    origin: process.env.CLIENT_URL || 'http://localhost:3000' || 'http://172.21.80.1:3000',
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT","DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
